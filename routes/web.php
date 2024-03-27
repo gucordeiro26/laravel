d3915145f667ee924075c0fa969d4dfa6028e69b
gucordeiro26 <gucordeiro26@gmail.com>
2024-03-27 18:11:47 -0300

    configurando o laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal']);
+
+Route::get('/sobrenos', [App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato']);
